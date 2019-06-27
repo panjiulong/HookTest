e3da7068f515c8b0321e2b8cd71d318187d201f4
Zachry <panjiulong@utimes.cc>
2019-06-27 22:06:37 +0800

    test13

--- ReaseTest/ReaseTest/ViewController.swift
+++ ReaseTest/ReaseTest/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let a = 20442
+    let a = 20441
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
